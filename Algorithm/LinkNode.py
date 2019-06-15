0cbd87e2ea883a756c9011ae59cfb554e3b2d703
JPMike <jpmikemessage@gmail.com>
2019-06-15 11:11:34 +0800

    modify linknode

--- Algorithm/LinkNode.py
+++ Algorithm/LinkNode.py
@@ -1,22 +1,10 @@
-import random
-
-
 class LinkNode:
     def __init__(self, data=None, next=None):
         self.data = data
         self.next = next
 
     def __str__(self):
-        return "{}->".format(self.get_data())
-
-    def get_data(self):
-        return self.data
-
-    def get_next(self):
-        return self.next
-
-    def set_next(self, next):
-        self.next = next
+        return "{}->".format(self.data)
 
 
 class DoublyLinkNode:
@@ -32,7 +20,7 @@ class LinkList:
 
     def insert(self, data):
         new_node = LinkNode(data)
-        new_node.set_next(self.head)
+        new_node.next = self.head
         self.head = new_node
 
     def size(self):
@@ -40,56 +28,58 @@ class LinkList:
         count = 0
         while curr:
             count += 1
-            curr = curr.get_next()
+            curr = curr.next
         return count
 
     def search(self, data):
         curr = self.head
+        while curr:
+            if curr.data == data:
+                break
+            else:
+                curr = curr.next
         if not curr:
             raise ValueError("Data not in list")
-        found = False
-        while curr and not found:
-            if curr.get_data() == data:
-                found = True
-            else:
-                curr = curr.get_next()
         return curr
 
     def delete(self, data):
+        # delete first found data
         curr = self.head
-        if not curr:
-            raise ValueError("Data not in list")
         prev = None
         while curr:
-            if curr.get_data() == data:
+            if curr.data == data:
                 if prev:
-                    prev.set_next(curr.get_next())
+                    prev.next = curr.next
                 else:
-                    self.head = curr.get_next()
+                    # delete head
+                    self.head = curr.next
                 break
             else:
                 prev = curr
-                curr = curr.get_next()
+                curr = curr.next
+        if not curr:
+            raise ValueError("Data not in list")
 
     def print(self):
         tmp = self.head
         while tmp:
             print(tmp, end="")
-            tmp = tmp.get_next()
+            tmp = tmp.next
         print("")
 
     @classmethod
     def create(cls, node_num):
         head = LinkNode()
-        tmp = head
-        for _ in range(node_num):
-            tmp.set_next(LinkNode(random.randint(1, node_num)))
-            tmp = tmp.get_next()
-        return LinkList(head.get_next())
+        # copy and reserve head
+        curr = head
+        for num in range(node_num):
+            curr.next = LinkNode(num)
+            curr = curr.next
+        return LinkList(head.next)
 
 
 if __name__ == "__main__":
-    link_list = LinkList.create(9)
+    link_list = LinkList.create(10)
     print("origin:")
     link_list.print()
     print("size: {}".format(link_list.size()))
