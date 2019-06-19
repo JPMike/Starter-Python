250a6345333edb5cddbe863879d62bef5a393160
JPMike <jpmikemessage@gmail.com>
2019-06-19 20:50:32 +0800

    daemon thread

--- StandardLibrary/threading-example/threading_daemon.py
+++ StandardLibrary/threading-example/threading_daemon.py
@@ -6,11 +6,13 @@ import logging
 def daemon():
     logging.debug('Starting')
     time.sleep(0.2)
+    print("daemon", file=open('threading_daemon_output.txt', 'a'))
     logging.debug('Exiting')
 
 
 def non_daemon():
     logging.debug('Staring')
+    print("non_daemon", file=open('threading_daemon_output.txt', 'a'))
     logging.debug('Exiting')
 
 
@@ -29,6 +31,7 @@ if __name__ == '__main__':
     # daemon thread not end before the main thread exit
     # let the main thread wait for the daemon thread to end
     # join will block the main thread indefinitely
+    # if not join, daemon thread will be killed, and never complete the output
     d.join()
     # give join a expire time to stop blocking
     # d.join(0.1)
